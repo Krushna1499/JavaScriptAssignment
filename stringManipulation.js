2d96a57b1cc7f7ef3acc7c4b0cce09bab1c5caf7
Krishna <lkrushana@gmail.com>
2024-05-07 14:36:43 +0530

    Completed Capatalize,CapatalizeAll,isExist

--- stringManipulation.js
+++ stringManipulation.js
@@ -1,12 +1,46 @@
-function captalize (sentence) {
+export function captalize (sentence) {
     let str =sentence.split(" ")
-    for(let i=0;i<str.length;i++)
-        {
-          
-            str[i]=str[i][1].toUpperCase()+str[i].substring(1);
-        }
-        return str.join(" ");
-   
+   str= str.map(item=>
+    {
+        return item[0].toUpperCase()+item.slice(1);
+
+    })
+    return str.join(" ")
 }
-console.log(captalize("i love Myself"));
-console.log(captalize("training centre"));
+
+
+
+
+
+
+export function captalizeAll (sentence1) {
+    let str =sentence1.split("")                     //convert string into array
+    
+    for(let i =0;i<str.length;i++)
+        {
+            str[i]=String.fromCharCode(str[i].charCodeAt(0)-32)
+
+        }
+        return str.join("")   //convert array into string
+
+}
+
+export function isExist(str){
+    let st =str.includes("Onkar")
+
+    if(st)
+        {
+            console.log("it is Available");
+    
+        }
+        else
+        {
+            console.log("It is not Available");
+        }
+}
+
+export function getOccurrenceCount()
+{
+    let strr = st
+}
+
